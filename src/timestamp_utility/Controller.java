8e9b31ed05a8e56962ea4a1b86028500b32e5a3e
Shapiro <JUSTIN.SHAPIRO@UCDENVER.EDU>
2017-01-16 15:08:24 -0700

    v.2

--- src/timestamp_utility/Controller.java
+++ src/timestamp_utility/Controller.java
@@ -12,7 +12,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
@@ -21,14 +20,15 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML private TextField output_file_box = new TextField();
-    @FXML private TextArea cik_box = new TextArea();
-    @FXML private TextArea filing_type_box = new TextArea();
-    @FXML private TextArea date_box = new TextArea();
-    @FXML private Button browse_button = new Button();
-    @FXML private Button start_button = new Button();
-    @FXML private Label status_indicator = new Label();
-    @FXML private Hyperlink view_result = new Hyperlink();
+    @FXML private TextField output_file_box;
+    @FXML private TextArea cik_box;
+    @FXML private TextArea filing_type_box;
+    @FXML private TextArea date_box;
+    @FXML private Button browse_button;
+    @FXML private Button start_button;
+    @FXML private Label status_indicator;
+    @FXML public Label progress_area;
+    @FXML private Hyperlink view_result;
 
     private File result_file = new File(System.getProperty("user.dir") + "\\timestamp_result.csv");
 
@@ -70,43 +70,53 @@ public class Controller implements Initializable {
                 alert.setContentText("The \"Disclosure Accepted:\" box must contain a value.");
                 alert.showAndWait();
             } else {
-                output_file_box.setDisable(true);
-                browse_button.setDisable(true);
-                start_button.setDisable(true);
-                cik_box.setDisable(true);
-                filing_type_box.setDisable(true);
-                date_box.setDisable(true);
-                status_indicator.setTextFill(Color.web("#aa7243"));
-                status_indicator.setText("Running...");
-                view_result.setVisible(false);
-
                 String[] cik_data = cik_box.getText().split("\n");
                 String[] filing_type_data = filing_type_box.getText().split("\n");
                 String[] date_data = date_box.getText().split("\n");
                 final String[][] data = {cik_data, filing_type_data, date_data};
 
-                new Thread(new Task<Void>() {
-                    @Override
-                    protected Void call() throws Exception {
-                        SECTimestampCollector collector_agent = new SECTimestampCollector();
-                        collector_agent.init(output_file_box.getText(), data);
-                        result_file = collector_agent.collect();
+                if (!((cik_data.length == filing_type_data.length) && (cik_data.length == date_data.length))) {
+                    alert.setContentText("The number of elements in each box must be the same.");
+                    alert.showAndWait();
+                } else {
+                    output_file_box.setDisable(true);
+                    browse_button.setDisable(true);
+                    start_button.setDisable(true);
+                    cik_box.setDisable(true);
+                    filing_type_box.setDisable(true);
+                    date_box.setDisable(true);
+                    progress_area.setDisable(false);
+                    progress_area.setVisible(true);
+                    status_indicator.setTextFill(Color.web("#aa7243"));
+                    status_indicator.setText("Running...");
+                    view_result.setVisible(false);
 
-                        Platform.runLater(() -> {
-                            output_file_box.setDisable(false);
-                            browse_button.setDisable(false);
-                            status_indicator.setTextFill(Color.web("#328332"));
-                            status_indicator.setText("Job Finished");
-                            start_button.setDisable(false);
-                            view_result.setVisible(true);
-                            cik_box.setDisable(false);
-                            filing_type_box.setDisable(false);
-                            date_box.setDisable(false);
-                        });
 
-                        return null;
-                    }
-                }).start();
+                    new Thread(new Task<Void>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            SECTimestampCollector collector_agent = new SECTimestampCollector();
+                            collector_agent.init(output_file_box.getText(), data, Controller.this);
+                            result_file = collector_agent.collect();
+
+                            Platform.runLater(() -> {
+                                output_file_box.setDisable(false);
+                                browse_button.setDisable(false);
+                                status_indicator.setTextFill(Color.web("#328332"));
+                                status_indicator.setText("Job Finished");
+                                start_button.setDisable(false);
+                                view_result.setVisible(true);
+                                cik_box.setDisable(false);
+                                filing_type_box.setDisable(false);
+                                date_box.setDisable(false);
+                                progress_area.setDisable(true);
+                                progress_area.setVisible(false);
+                            });
+
+                            return null;
+                        }
+                    }).start();
+                }
             }
         });
 
--- src/timestamp_utility/SECTimestampCollector.java
+++ src/timestamp_utility/SECTimestampCollector.java
@@ -1,11 +1,13 @@
 package timestamp_utility;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 class SECTimestampCollector {
     private String output_dir;
@@ -13,12 +15,14 @@ class SECTimestampCollector {
     private String[] filing_data;
     private String[] date_data;
     private final String base_url = "https://sec.gov/Archives/";
+    private Controller ui;
 
-    void init(String _output_dir, String[][] box_data) {
+    void init(String _output_dir, String[][] box_data, Controller _ui) {
         output_dir = _output_dir;
         cik_data = box_data[0];
         filing_data = box_data[1];
         date_data = box_data[2];
+        ui = _ui;
     }
 
     File collect() {
@@ -26,49 +30,52 @@ class SECTimestampCollector {
 
         try {
             PrintWriter CSV = new PrintWriter(result_file);
-            CSV.println("CIK,Disclosure,Disclosure Accepted,Last Modified");
-            CSV.close();
-            int max_len = Math.max(Math.max(cik_data.length, filing_data.length), date_data.length);
+            CSV.println("CIK,Disclosure,Disclosure Accepted,Last Modified,HTML Accepted"); CSV.close();
+
+            int max_len = cik_data.length;
             for (int i = 0; i < max_len; i++) {
+                Boolean found = false;
                 if (!filing_data[i].equals("Press Release")) {
                     String csv_line = cik_data[i] + "," + filing_data[i] + "," + date_data[i];
                     String url_str = dateToIDX_URL(date_data[i]);
                     URL master_url = new URL(url_str);
-                    System.out.println("For " + date_data[i] + ", Reading " + url_str);
-                    BufferedReader master_file = new BufferedReader(new InputStreamReader(master_url.openStream()));
+                    BufferedReader master = new BufferedReader(new InputStreamReader(master_url.openStream()));
                     Boolean start_running = false;
 
-                    for (String line = master_file.readLine(); line != null; line = master_file.readLine()) {
-                        String[] current_line = line.split("\\|");
-                        if (start_running || current_line.length == 5) {
+                    for (String line = master.readLine(); line != null; line = master.readLine()) {
+                        String[] line_arr = line.split("\\|");
+                        if (start_running || line_arr.length == 5) {
                             if (!start_running) {
-                                master_file.readLine();
-                                current_line = master_file.readLine().split("|");
+                                master.readLine();
+                                line_arr = master.readLine().split("\\|");
                                 start_running = true;
                             }
 
-                            String curr_cik = current_line[0];
-                            if (curr_cik.equals(cik_data[i])) {
-                                String curr_filing_type = current_line[2];
-                                if (curr_filing_type.equals(filing_data[i])) {
-                                    String curr_date = dateParse(current_line[3]);
-                                    if (curr_date.equals(date_data[i].split(" ")[0])) {
-                                        System.out.print("-> Found filing in masters: ");
-                                        URL filing_url = new URL(base_url + current_line[4]);
-                                        csv_line += "," + getTimestamp(filing_url);
-                                    }
+                            if (line_arr[0].equals(cik_data[i]) && line_arr[2].equals(filing_data[i])) {
+                                if (dateParse(line_arr[3], true).equals(date_data[i].split(" ")[0])) {
+                                    URL filing_url = new URL(base_url + line_arr[4]);
+                                    csv_line += "," + getTimestamp(filing_url);
+
+                                    String accession_num = line_arr[4].replaceAll(".txt", "");
+                                    String submission_url_str = base_url + "edgar/data/" + line_arr[0] + "/";
+                                    submission_url_str +=  accession_num.replaceAll("-", "") + "/";
+                                    submission_url_str += accession_num + "-index.htm";
+                                    URL submission_url = new URL(submission_url_str);
+                                    csv_line += "," + getAcceptedDate(submission_url);
+
+                                    found = true;
                                 }
                             }
                         }
+                        if (found) break;
                     }
                     CSV = new PrintWriter(new FileOutputStream(result_file, true));
-                    CSV.println(csv_line);
-                    CSV.close();
+                    CSV.println(csv_line); CSV.close();
                 }
 
-                float progress = ((float) i / (float) max_len) * 100;
-                System.out.println(progress + "% complete");
+                send_progress_to_ui(((float) i / (float) max_len) * 100);
             }
+
         } catch (IOException e) { /* ignore */ }
         return result_file;
     }
@@ -102,11 +109,8 @@ class SECTimestampCollector {
             }
         }
 
-        if (year_idx == 2) {
-            month_idx = 0;
-        } else {
-            month_idx = 1;
-        }
+        if (year_idx == 2) month_idx = 0;
+        else month_idx = 1;
 
         String month_str = basic_date[month_idx];
         int month = Integer.parseInt(month_str);
@@ -119,34 +123,34 @@ class SECTimestampCollector {
 
     private int get_qtr_num(int month) {
         int qtr_num = 0;
-        if (month < 4) {
-            qtr_num = 1;
-        } else if (month >= 4 && month < 7) {
-            qtr_num = 2;
-        } else if (month >= 7 && month < 10) {
-            qtr_num = 3;
-        } else if (month >= 10 && month < 13) {
-            qtr_num = 4;
-        }
+
+        if (month < 4) qtr_num = 1;
+        else if (month >= 4 && month < 7) qtr_num = 2;
+        else if (month >= 7 && month < 10) qtr_num = 3;
+        else if (month >= 10 && month < 13) qtr_num = 4;
 
         return qtr_num;
     }
 
-    private String dateParse(String raw_date) {
+    private String dateParse(String raw_date, Boolean reorder) {
         String parsed = raw_date.replaceAll("-", "/");
         String[] new_date = parsed.split("/");
-        if (new_date[1].charAt(0) == '0') {
-            new_date[1] = new_date[1].substring(1);
-        }
-        if (new_date[2].charAt(0) == '0') {
-            new_date[2] = new_date[2].substring(1);
-        }
-        String actual_date = new_date[1];
-        actual_date += "/" + new_date[2];
-        actual_date += "/" + new_date[0];
-        parsed = actual_date;
 
-        return parsed;
+        short month_idx, day_idx, year_idx;
+        if (reorder) {
+            month_idx = 1;
+            day_idx = 2;
+            year_idx = 0;
+        } else {
+            month_idx = 0;
+            day_idx = 1;
+            year_idx = 2;
+        }
+
+        if (new_date[month_idx].charAt(0) == '0') new_date[month_idx] = new_date[month_idx].substring(1);
+        if (new_date[day_idx].charAt(0) == '0') new_date[day_idx] = new_date[day_idx].substring(1);
+
+        return new_date[month_idx] + "/" + new_date[day_idx] + "/" + new_date[year_idx];
     }
 
     private String getTimestamp(URL u) {
@@ -155,35 +159,48 @@ class SECTimestampCollector {
         try {
             HttpsURLConnection url_stream = (HttpsURLConnection) u.openConnection();
 
-            Date date = new Date(url_stream.getLastModified());
-            upload_timestamp = new SimpleDateFormat("MM-dd-yyyy HH:mm").format(date);
+            Calendar c = Calendar.getInstance();
+            c.setTime(new Date(url_stream.getLastModified()));
+            c.add(Calendar.HOUR_OF_DAY, 2);
+            upload_timestamp = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss").format(c.getTime());
 
-            String[] date_arr = upload_timestamp.split(" ")[0].split("-");
-            if (date_arr[0].charAt(0) == '0') {
-                date_arr[0] = date_arr[0].substring(1);
-            }
-            if (date_arr[1].charAt(0) == '0') {
-                date_arr[1] = date_arr[1].substring(1);
-            }
-
-            String[] time = upload_timestamp.split(" ")[1].split(":");
-            int mdt_mst_conversion = Integer.parseInt(time[0]) + 2;
-            if (mdt_mst_conversion > 24) {
-                mdt_mst_conversion -= 2;
-                if (mdt_mst_conversion == 23) {
-                    mdt_mst_conversion = 1;
-                } else if (mdt_mst_conversion == 24) {
-                    mdt_mst_conversion = 2;
-                }
-            }
-            time[0] = Integer.toString(mdt_mst_conversion);
-
-            String formatted_timestamp = date_arr[0] + "/" + date_arr[1];
-            formatted_timestamp += "/" + date_arr[2] + " " + time[0] + ":" + time[1];
-            upload_timestamp = formatted_timestamp;
-            System.out.println("Formatted timestamp: " + upload_timestamp);
+            String time = upload_timestamp.split(" ")[1];
+            upload_timestamp = dateParse(upload_timestamp.split(" ")[0].replaceAll("-", "/"), false);
+            upload_timestamp += " " + time;
         } catch (IOException e) { /* ignore */ }
 
         return upload_timestamp;
+    }
+
+    private String getAcceptedDate(URL u) {
+        String date_line = "";
+        try {
+            BufferedReader r = new BufferedReader(new InputStreamReader(u.openStream()));
+            for (String line = r.readLine(); line != null; line = r.readLine()) {
+                if (line.contains("Filing Date")) {
+                    r.readLine(); r.readLine();
+                    date_line = r.readLine();
+                    break;
+                }
+            }
+        } catch (IOException e) { /* ignore */ }
+
+        String full_date = date_line.split(">")[1].split("<")[0];
+        date_line = dateParse(full_date.split(" ")[0], true) + " " + full_date.split(" ")[1];
+
+        return date_line;
+    }
+
+    private void send_progress_to_ui(float percent_complete) {
+        new Thread(new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    ui.progress_area.setText(String.format("%.2f%%", percent_complete));
+                });
+
+                return null;
+            }
+        }).start();
     }
 }
